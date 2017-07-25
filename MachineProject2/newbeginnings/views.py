a36b18a564b6ca91f6ec1fe7017acdf27d7541d2
kiefer12 <charles_chong@dlsu.edu.ph>
2017-07-26 00:29:56 +0800

    minor fix
    minor fix

--- MachineProject2/newbeginnings/views.py
+++ MachineProject2/newbeginnings/views.py
@@ -31,28 +31,7 @@ class Register(generic.CreateView):
         form =self.form_class(None)
         return render(request, self.template_name, {'form':form})
     
-    
-    #process from data
-    def post(self,request):
-        form = self.form_class(request.POST)
-            
-        if form.is_valid():
-            
-            user = form.save(commit=False)
-            
-            #cleaned(normalized) data
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.save()
-            
-            #returns User objects if credentials are correct 
-            user = authenticate(username=username, password=password)
-            
-            if user is not None:
-                login(request,user)
-                return HttpResponseRedirect('/home/')
-                    
-        return render(request, self.template_name, {'form':form}) 
+
     
 
 def login_view(request):
