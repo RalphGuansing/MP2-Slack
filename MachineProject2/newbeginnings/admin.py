3460b97da2e0228542a61ef836eb5d3809a1d8f3
Ralph Guansing <ralph_guansing@dlsu.edu.ph>
2017-07-31 13:43:38 +0800

    Models for MP3

--- MachineProject2/newbeginnings/admin.py
+++ MachineProject2/newbeginnings/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Offer
 
+
+admin.site.register(Offer)
 admin.site.register(Post)
--- MachineProject2/newbeginnings/models.py
+++ MachineProject2/newbeginnings/models.py
@@ -27,3 +27,15 @@ class Post(models.Model):
     
     def __str__(self):
         return self.item_name +' ('+str(self.id)+') '
+    
+class Offer(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post,on_delete=models.CASCADE, related_name ="original_post")
+    isPurchase = models.BooleanField(default=True)
+    purchase_offer = models.IntegerField(default=0, blank=True)
+    exchange_offer = models.ForeignKey(Post,on_delete=models.CASCADE, related_name="exchange_post", blank=True,null=True)
+    isAccept = models.BooleanField(default=False) 
+    
+    def __str__(self):
+        return "offer of " + self.user_id.username +' to post '+str(self.post_id)+' (' +str(self.id)+") "
+    
